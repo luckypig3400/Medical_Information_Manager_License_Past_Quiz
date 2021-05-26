da47ddaa07be31f986720a6645e753064419892e
郵差 <twHenryYang@gmail.com>
2021-05-26 08:06:02 +0800

    replace line break characters with <br>

--- readCSV.js
+++ readCSV.js
@@ -9,6 +9,9 @@ function readTextFile(file) {
         if (rawFile.readyState === 4) {
             if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
+                allText = allText.replace(/(?:\r\n|\r|\n)/g,'<br>');
+                //轉換換行符號為HTML的<br>
+                //https://stackoverflow.com/questions/784539/how-do-i-replace-all-line-breaks-in-a-string-with-br-elements
                 contentParagraph.innerHTML = allText;
             }
         }
