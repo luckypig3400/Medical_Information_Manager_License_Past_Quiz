bd0d7b9fd792a7afe91914986e50a286e295850f
郵差 <a34007024@gmail.com>
2021-10-08 16:47:13 +0800

    extract page

--- past_exam_questions_csv/fetchPage.py
+++ past_exam_questions_csv/fetchPage.py
@@ -1,5 +1,5 @@
 import csv
-with open('202011.csv', newline='') as csvfile:
+with open('202004.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     line_count = 0
     for row in csvreader:
@@ -10,5 +10,11 @@ with open('202011.csv', newline='') as csvfile:
             #print(line_count)
             line_count += 1
             data = row[1]  # 抓取每列的第二欄資料
-            page = data.find("？")
-            print(data[page+1:])  # 從找到問號的索引+1輸出到結尾
+            page = data.find("(*")
+            # print(data[page+1:])  # 從找到問號的索引+1輸出到結尾
+            pageText = data[page+1:]
+            pageText = pageText.replace(")","")
+            pageText = pageText.replace("**","常用醫護術語")
+            pageText = pageText.replace("*","")
+            # 在這邊寫章節判斷
+            print(pageText)
