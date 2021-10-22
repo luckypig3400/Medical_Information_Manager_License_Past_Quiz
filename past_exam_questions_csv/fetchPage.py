c1afd8357e28eaca8ffac3a268cf78c2084825c4
郵差 <a34007024@gmail.com>
2021-10-22 22:43:33 +0800

    fetch and clean up reference page data

--- past_exam_questions_csv/fetchPage.py
+++ past_exam_questions_csv/fetchPage.py
@@ -1,5 +1,5 @@
 import csv
-with open('202004.csv', newline='') as csvfile:
+with open('200712.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     line_count = 0
     for row in csvreader:
@@ -12,9 +12,9 @@ with open('202004.csv', newline='') as csvfile:
             data = row[1]  # 抓取每列的第二欄資料
             page = data.find("(*")
             # print(data[page+1:])  # 從找到問號的索引+1輸出到結尾
-            pageText = data[page+1:]
-            pageText = pageText.replace(")","")
-            pageText = pageText.replace("**","常用醫護術語")
-            pageText = pageText.replace("*","")
+            pageText = data[page:]
+            #pageText = pageText.replace(")","")
+            #pageText = pageText.replace("**","常用醫護術語")
+            #pageText = pageText.replace("*","")
             # 在這邊寫章節判斷
             print(pageText)
