dc10c2adf9e441cb078988bc8445c8491975fe8d
郵差 <a34007024@gmail.com>
2021-10-25 10:12:52 +0800

    process multiple pages special charaters

--- past_exam_questions_csv/chapterClassify.py
+++ past_exam_questions_csv/chapterClassify.py
@@ -1,4 +1,19 @@
 import csv
+
+
+def proccessMultiPage(pageTextIn):
+    if(pageTextIn.find("~") != -1):
+        return pageTextIn[0:pageTextIn.find("~")]
+    elif(pageTextIn.find("-") != -1):
+        return pageTextIn[0:pageTextIn.find("-")]
+    elif(pageTextIn.find("、") != -1):
+        return pageTextIn[0:pageTextIn.find("、")]
+    elif(pageTextIn.find(",") != -1):
+        return pageTextIn[0:pageTextIn.find(",")]
+    else:
+        return pageText  # Can't Find any special characters
+
+
 with open('202011.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     line_count = 0
@@ -11,10 +26,21 @@ with open('202011.csv', newline='') as csvfile:
             line_count += 1
             data = row[7]  # 抓取每列的第八欄資料
             page = data.find("(")
+            if(page == -1):  # 保留自己標記的null
+                page = 0
             pageText = data[page:]  # 從找到前括號的索引輸出到結尾
             pageText = pageText.replace(")", "")
             pageText = pageText.replace("(", "")
             pageText = pageText.replace("**", "常用醫護術語")
             pageText = pageText.replace("*", "")
-            print(pageText)
-            # 在這邊寫章節判斷
+            # print(pageText)
+            try:
+                x = pageText.find("常用")
+                if(x == -1):  # 非常用醫護術語再進一步處理
+                    pageText = proccessMultiPage(pageText)  # 處理多頁數標記
+                    pageInt = (int)(pageText)
+                    # 在這邊寫章節判斷
+                else:
+                    print(pageText)
+            except ValueError:
+                print(pageText)  # Oops not able to convert to integer
