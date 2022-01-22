dcee14a44a5880106c198fbe3bf51e27b6d15414
郵差 <twHenryYang@gmail.com>
2022-01-22 14:36:17 +0800

    Fetch DB data as JSON format

--- model/fetchPublicDB.php
+++ model/fetchPublicDB.php
@@ -1,7 +1,5 @@
 <?php
 
-use function PHPSTORM_META\type;
-
 require("./config.php");
 
 try {
@@ -9,10 +7,16 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql_selectCommand = "SELECT * FROM `publicq` ORDER BY `publicq`.`publicID` ASC";
+    // Select MySQL as JSON: https://stackoverflow.com/questions/41758870/how-to-convert-result-table-to-json-array-in-mysql
+    $sql_selectAsJSONCommand = "SELECT JSON_OBJECT('publicID',publicID,
+    'Correct_Answers',Correct_Answers,'Question',Question,'Answer1',Answer1,
+    'Answer2',Answer2,'Answer3',Answer3, 'Answer4',Answer4,'Year',Year,
+    'Ref_Page',Ref_Page,'Chapter',Chapter,'Detail1',Detail1,'Detail2',Detail2,
+    'Detail3',Detail3,'Detail4',Detail4) FROM `publicq`;";
 
     // Select data from DB with PDO method:
     // https://www.w3schools.com/php/php_mysql_select.asp
-    $stmt = $conn->prepare($sql_selectCommand);
+    $stmt = $conn->prepare($sql_selectAsJSONCommand);
     $stmt->execute();
 
     // set the resulting array to associative
@@ -20,12 +24,11 @@ try {
 
     foreach ($stmt->fetchAll() as $row) {
         // print_r($row);
-        echo "<p>";
         foreach ($row as $columnName => $cellData) {
             // 要使用這種包含索引建的查詢方式，前面setFetchMode(PDO::FETCH_ASSOC)是關鍵
-            echo "<b>$columnName:</b>".$row[$columnName] . "&#9;";
+            echo $row[$columnName] . "&#9;";
         }
-        echo "</p>";
+        echo ",";
     }
 } catch (PDOException $e) {
     echo "<b>Error:</b>" . $e->getMessage();
