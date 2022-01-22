2bb246e4217b6f040c624db928e32506d5daf8b9
郵差 <twHenryYang@gmail.com>
2022-01-22 13:07:51 +0800

    select data as normal text

--- model/fetchPublicDB.php
+++ model/fetchPublicDB.php
@@ -1,12 +1,42 @@
 <?php
+
+use function PHPSTORM_META\type;
+
 require("./config.php");
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    $sql_selectCommand = "SELECT * FROM `publicq` ORDER BY `publicq`.`publicID` ASC";
+    // Select data from DB with PDO method:
+    // https://www.w3schools.com/php/php_mysql_select.asp
+    $stmt = $conn->prepare($sql_selectCommand);
+    $stmt->execute();
 
+    foreach ($stmt->fetchAll() as $row) {
+        // print_r($row);
 
-// Select MySQL as JSON: https://stackoverflow.com/questions/41758870/how-to-convert-result-table-to-json-array-in-mysql
+        echo "<p>";
+        for ($i = 0; $i < sizeof($row) / 2; $i++) {
+            echo $row[$i] . "&#9;";
+        }
+        // foreach ($row as $columnName => $cellData) {
+        //     echo $row[$columnName] . "&#9;";
+        // }
+        echo "</p>";
+    }
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    // Select MySQL as JSON: https://stackoverflow.com/questions/41758870/how-to-convert-result-table-to-json-array-in-mysql
+
+} catch (PDOException $e) {
+    echo "<b>Error:</b>" . $e->getMessage();
+}
+
+$conn = null;
 
 ?>
 
